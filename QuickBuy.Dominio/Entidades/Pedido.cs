83c8af598d562b47cc1b2712d13d8ba3c5c1da7c
tammenhain123 <tammerafael@hotmail.com>
2020-03-03 20:29:08 -0300

    Configuração Dominio e Add do Enum

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,26 @@
+﻿using QuickBuy.Dominio.ObjetoValor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+        //deve ter pelo menos um item de pedido ou vários itens
+        public ICollection<ItemPedido> ItensPedido { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public int NumeroEndereco { get; set; }
+        public int FormaPagamentoId { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
+
+    }
+}
---
+++ QuickBuy.Dominio/ObjetoValor/FormaPagamento.cs
@@ -0,0 +1,31 @@
+﻿using QuickBuy.Dominio.Enumerados;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.ObjetoValor
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public bool VerificaBoleto
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Boleto; }
+        }
+        public bool VerificaCartao
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.CartaoCredito; }
+        }
+        public bool VerificaDeposito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Deposito; }
+        }
+        public bool VerificaNaoDefinido
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+        }
+    }
+}
